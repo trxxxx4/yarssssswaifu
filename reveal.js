3aff0e1f458b85677cc8df0bc5b2db4a58268c80
trxxxx4 <149764131+trxxxx4@users.noreply.github.com>
2024-02-08 16:06:25 +0500

    Add files via upload

--- reveal.js
+++ reveal.js
@@ -1,7 +1,24 @@
-const anime = '<3';
-console.log(anime);
+function revealFunction(){
+    window.sr = ScrollReveal({
+        duration: 1350, 
+    });
+    sr.reveal('#a__header__links', {origin: 'top', distance: '250px', reset: true});
+    sr.reveal('#logo', {reset:true, duration: 1000, origin:"top", distance: '250px'});
+    sr.reveal('#character__text__container', { reset: true, origin: 'left', distance: '500px'});
+    sr.reveal('#character__image', {origin:"right" , distance: '100px'});
+    sr.reveal('#footer__links__container', {reset: true, distance: "250px", duration: 1000, origin: 'left'});
+    sr.reveal('#anime__watch__link', {reset: true, distance: "200px", duration: 1000, origin: 'bottom'});
+    sr.reveal('#anime__title__container', {reset:true, distance: '400px', duration:1000, origin: 'left'});
+}
 
-const sr = ScrollReveal({
-    reset: true
+
+
+
+
+
+
+
+
+window.addEventListener('load', () =>{
+    revealFunction();
 })
-sr.reveal('#header__logo', {delay: 400, origin: 'left'});
