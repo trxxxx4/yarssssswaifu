9b45726f0035e1f9f71cbd2de3fec8e4eac719ab
trxxxx4 <149764131+trxxxx4@users.noreply.github.com>
2024-02-08 16:22:37 +0500

    Add files via upload

--- reveal.js
+++ reveal.js
@@ -5,10 +5,10 @@ function revealFunction(){
     sr.reveal('#a__header__links', {origin: 'top', distance: '250px', reset: true});
     sr.reveal('#logo', {reset:true, duration: 1000, origin:"top", distance: '250px'});
     sr.reveal('#character__text__container', { reset: true, origin: 'left', distance: '500px'});
-    sr.reveal('#character__image', {origin:"right" , distance: '100px'});
-    sr.reveal('#footer__links__container', {reset: true, distance: "250px", duration: 1000, origin: 'left'});
-    sr.reveal('#anime__watch__link', {reset: true, distance: "200px", duration: 1000, origin: 'bottom'});
-    sr.reveal('#anime__title__container', {reset:true, distance: '400px', duration:1000, origin: 'left'});
+    sr.reveal('#character__image', {reset: false, origin:"right" , distance: '100px'});
+    sr.reveal('#footer__links__container', {reset: false, distance: "250px", duration: 1000, origin: 'left'});
+    sr.reveal('#anime__watch__link', {reset: false, distance: "200px", duration: 1000, origin: 'bottom'});
+    sr.reveal('#anime__title__container', {reset: false, distance: '400px', duration:1000, origin: 'left'});
 }
 
 
